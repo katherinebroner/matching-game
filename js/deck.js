172d6ff24cb191e4f183c3645b833c459f4cc23f
Katherine Broner <katherine.broner@gmail.com>
2016-09-08 13:11:56 -0400

    write createNumbers function

--- js/deck.js
+++ js/deck.js
@@ -2,3 +2,13 @@ function Deck () {
   this.cards = [];
 }
 
+Deck.prototype.createNumbers = function() {
+  var nums = [1,2,3,4,5,6,7,8,9,10];
+  var duplicate = _.clone(nums);
+  var combine = _.concat(nums, duplicate);
+  return combine;
+};
+
+var deck = new Deck;
+var nums = deck.createNumbers();
+console.log(nums);
